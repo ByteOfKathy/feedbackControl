4825438629f370ded5e25d3f58aa1dce570c238e
kathy chan <katriana73@yahoo.com>
2020-01-11 18:35:03 -0500

    updated example1

--- example1.py
+++ example1.py
@@ -44,7 +44,23 @@ def open_loop(p, tm=5000):
         u = target(t)
         y = p.work(u)
 
-        print t, u, 0, u, y
+        print(t, u, 0, u, y)
 
 def closed_loop(c, p, tm=5000):
-    def setpoint()
+    def setpoint(t):
+        if t < 100: return 0
+        if t < 300: return 50
+        return 10
+
+    y = 0
+    for t in range(tm):
+        r = setpoint(t)
+        e = r - y
+        u = c.work(e)
+        y = p.work(u)
+
+        print(t, r, e, u, y)
+    
+    c = Controller(1.25, 0.01)
+    p = []
+    open_loop(p, 1000)
